1659e5b1d0af47dc10a90a2ea9199d40bfa24ca4
Ethan-Heimer <111470266+Ethan-Heimer@users.noreply.github.com>
2023-09-08 23:37:15 -0700

    Delete routes/TestRoutes.js

--- routes/TestRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const TestControlls = require("../controllers/testcontrolls.js");
-
-router.get('/', TestControlls.GetAllUsers);
-
-module.exports = router;
