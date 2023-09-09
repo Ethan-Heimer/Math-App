439455af5b5c01ff83ff6015dc940363a3ce6aec
Ethan-Heimer <111470266+Ethan-Heimer@users.noreply.github.com>
2023-09-08 23:36:59 -0700

    Delete controllers/testcontrolls.js

--- controllers/testcontrolls.js
+++
@@ -1,9 +0,0 @@
-const model = require("../model/database.js");
-
-module.exports = {
-    GetAllUsers: (req, res) => {
-        model.GetAllUsers((users, error) => {
-            res.render("users", {users});
-        })
-    }
-}
